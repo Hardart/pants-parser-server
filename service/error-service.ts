e7001bf59a6e889b4a40c76e6fcd64bfb37b6bb4
Hardart <kaino@bk.ru>
2024-06-04 00:46:25 +0300

    some changes in Error service

--- service/error-service.ts
+++ service/error-service.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 export default class ErrorService {
-  private static basePath = process.env.NODE_ENV === 'production' ? '../logs/' : './logs/'
+  private static basePath = process.env.NODE_ENV === 'production' ? '../logs' : './logs'
   static get dateAndTime() {
     return Intl.DateTimeFormat('ru', {
       year: '2-digit',
@@ -17,12 +17,14 @@ export default class ErrorService {
       year: '2-digit',
       month: '2-digit',
       day: '2-digit'
-    }).format(new Date())
+    })
+      .format(new Date())
+      .replace('.', '')
   }
 
   static addError(error: unknown) {
     try {
-      fs.appendFileSync(`${this.basePath}errors.txt`, `${this.dateAndTime}: ${error}\n`, { encoding: 'utf-8' })
+      fs.appendFileSync(`${this.basePath}/errors.txt`, `${this.dateAndTime}: ${error}\n`, { encoding: 'utf-8' })
     } catch (error) {
       console.log(error)
     }
@@ -30,17 +32,19 @@ export default class ErrorService {
 
   static saveStream(streamTitle?: string) {
     try {
-      fs.appendFileSync(`${this.basePath}${this.date}_stream.txt`, `${this.dateAndTime}: ${streamTitle}\n`, { encoding: 'utf-8' })
+      fs.appendFileSync(`${this.basePath}/${this.date}_stream.txt`, `${this.dateAndTime}: ${streamTitle}\n`, {
+        encoding: 'utf-8'
+      })
     } catch (error) {
-      console.log(error)
+      this.addError(error)
     }
   }
 
   static saveEmpty(streamTitle?: string) {
     try {
-      fs.appendFileSync('./logs/empty.txt', `${this.dateAndTime}: Empty data ${streamTitle}\n`, { encoding: 'utf-8' })
+      fs.appendFileSync(`${this.basePath}/empty.txt`, `${this.dateAndTime}: Empty data ${streamTitle}\n`, { encoding: 'utf-8' })
     } catch (error) {
-      console.log(error)
+      this.addError(error)
     }
   }
 }
