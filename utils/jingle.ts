a19bb0ab82a6b544b47833650319d9c8a79e94b2
Hardart <kaino@bk.ru>
2024-06-05 00:10:44 +0300

    remove unused files

--- utils/jingle.ts
+++
@@ -1,8 +0,0 @@
-import { Track } from '../models/Track'
-
-export const jingleData: Omit<Track, 'createdAt' | 'updatedAt'> = {
-  artistName: 'Радио Штаны',
-  trackTitle: 'Прямой эфир',
-  cover: '/assets/simple_logo.svg',
-  preview: ''
-}
