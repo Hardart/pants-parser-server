00d88c6780a6b29ece4accc1e6bac00270d6f45b
Hardart <kaino@bk.ru>
2024-06-23 22:35:06 +0300

    small refactor

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import { Server } from 'socket.io'
+import { Server, Socket } from 'socket.io'
 import { connectToDB } from './mongo-connect'
 import { radioStation } from './parser'
 import { IcecastParser } from './utils/IcecastParser'
@@ -9,14 +9,11 @@ const ioProps = {
 }
 const io = new Server(3071, ioProps)
 
-io.on('connection', (socket) => {
-  if (CacheService.metaData.artistName !== '') socket.emit('meta', CacheService.metaData)
-  socket.on('user:connect', () => socket.emit('meta', CacheService.metaData))
-
-  // socket.on('host:online', ({ hostId, state }) => {
-  //   if (state) fns.findHost(hostId)
-  //   else io.emit('host:data', undefined)
-  // })
-})
 connectToDB()
 radioStation.on('metadata', (metadata) => IcecastParser.readMetadata(io, metadata))
+io.on('connection', onConnection)
+
+function onConnection(socket: Socket) {
+  if (CacheService.metaData.artistName !== '') socket.emit('meta', CacheService.metaData)
+  socket.on('user:connect', () => socket.emit('meta', CacheService.metaData))
+}
